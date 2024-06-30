df9673ca239cfb8ebeaf26274060d2677678fc8d
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-06-30 19:34:01 +0000

    creando parte de los modelos

--- src/models.py
+++ src/models.py
@@ -1,29 +1,37 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Date
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Planet(Base):
+    __tablename__ = 'planet'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True , autoincrement=True)
+    population = Column(Integer, nullable=False)
+    climate= Column(Enum("arid", "temperate", "tropical","frozen","murky"),nullable=False)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250), unique=True,nullable=False)
+    email = Column(String(250), unique=True,nullable=False)
+    password = Column(String(250), unique=True,nullable=False)
+    
+class Characters(Base):
+    __tablename__= "character"
+    id= Column(Integer, primary_key=True)
+    name=Column(String(250), nullable=False)
+    birth_year= Column(String(250), nullable=False)
+    gender = Column(String(50))
+    species= Column(String(20))
 
     def to_dict(self):
         return {}
