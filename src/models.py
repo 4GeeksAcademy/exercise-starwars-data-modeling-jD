01af44b68097ae1ac18d78ff9b9b3aa01198238e
jdOJ <162026593+jdOJ@users.noreply.github.com>
2024-07-07 16:53:45 +0000

    claro

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Date
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum, Date, func
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
@@ -13,7 +13,7 @@ class Planet(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True , autoincrement=True)
     population = Column(Integer, nullable=False)
-    climate= Column(Enum("arid", "temperate", "tropical","frozen","murky"),nullable=False)
+    climate= Column(Enum(),nullable=False)
     name = Column(String(250), nullable=False)
 
 class User(Base):
@@ -21,17 +21,34 @@ class User(Base):
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(250), unique=True,nullable=False)
-    email = Column(String(250), unique=True,nullable=False)
-    password = Column(String(250), unique=True,nullable=False)
+    name = Column(String(255), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+    date = Column(Date, default=func.current_date(), nullable=False)
+    
+    favorites = relationship("Favorite", back_populates="user")
+
+
+class Favorite(Base):
+    __tablename__ = "favorite"
+
+    id= Column(Integer, primary_key=True)
+    user_id = Column(Integer,ForeignKey("user.id"))
+    character_id= Column(Integer, ForeignKey("character.id"), nullable=True)
+    planet_id = Column(Integer,ForeignKey("planet.id"), nullable=True)
+
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character")
+    planet = relationship("Planet")
+
     
 class Characters(Base):
     __tablename__= "character"
     id= Column(Integer, primary_key=True)
     name=Column(String(250), nullable=False)
-    birth_year= Column(String(250), nullable=False)
+    birthday_year= Column(String(250), nullable=False)
     gender = Column(String(50))
-    species= Column(String(20))
+   
 
     def to_dict(self):
         return {}
